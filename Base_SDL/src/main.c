298cc42546dee1a759caa75600195955292cb074
DBorgondo <davidborgondo@outlook.com>
2020-11-13 18:44:13 +0100

    Ajout Gestion Entité
    Ajout d'une structure et de fonction pour la gestion des entités (Affichage et création).

--- Base_SDL/src/main.c
+++ Base_SDL/src/main.c
@@ -2,28 +2,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <SDL2/SDL_image.h>
+#define window_width 600
+#define window_height 600
+struct entity {
+    SDL_Texture* sprite;
+    SDL_Rect physic;
+};
+
+typedef struct entity Entity;
+
+SDL_Rect makeRect(int x, int y,int w,int h);
+SDL_Texture* createImage(const char *cheminFichier,SDL_Renderer* screen);
+Entity* createEntity(SDL_Renderer* screen,const char *cheminSprite,int x,int y,int w,int h);
+SDL_Point getSizeTexture(SDL_Texture *texture);
+void printEntity(SDL_Renderer* screen,Entity* entity);
+
 int main(int argc, char *argv[])
     {
-    SDL_Window* fenetre; // Déclaration de la fenêtre
-    SDL_Event evenements; // Événements liés à la fenêtre
-
+    SDL_Window* window;
+    SDL_Event evenements;
+    SDL_Renderer* screen;
     bool terminer = false;
-    if(SDL_Init(SDL_INIT_VIDEO) < 0) // Initialisation de la SDL
+    //Initialisation SDL
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf("Erreur d’initialisation de la SDL: %s",SDL_GetError());
 
         SDL_Quit();
         return EXIT_FAILURE;
     }
-    // Créer la fenêtre
-    fenetre = SDL_CreateWindow("Projet Base", SDL_WINDOWPOS_CENTERED,
-    SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_RESIZABLE);
-    if(fenetre == NULL) // En cas d’erreur
+
+    window = SDL_CreateWindow("Projet Base", SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED, window_width, window_height, SDL_WINDOW_RESIZABLE);
+    if(window == NULL)
     {
-        printf("Erreur de la creation d’une fenetre: %s",SDL_GetError());
+        printf("Erreur de la creation d’une window: %s",SDL_GetError());
         SDL_Quit();
         return EXIT_FAILURE;
     }
+    screen = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    //Initialisation Jeu
     // Boucle principale
     while(!terminer){
         SDL_PollEvent( &evenements );
@@ -43,7 +62,53 @@ int main(int argc, char *argv[])
             }
     }
     // Quitter SDL
-    SDL_DestroyWindow(fenetre);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
+
+SDL_Rect makeRect(int x, int y,int w,int h){
+    SDL_Rect rRect;
+    rRect.x=x;
+    rRect.y=y;
+    rRect.w=w;
+    rRect.h=h;
+    return rRect;
+}
+
+    SDL_Texture* createImage(const char *cheminFichier,SDL_Renderer* screen){
+        SDL_Surface* hSurface = IMG_Load(cheminFichier);
+        return SDL_CreateTextureFromSurface(screen,hSurface);
+    }
+
+SDL_Point getSizeTexture(SDL_Texture *texture){
+    SDL_Point size;
+    SDL_QueryTexture(texture, NULL, NULL, &size.x, &size.y);
+    return size;
+}
+
+Entity* createEntity(SDL_Renderer* screen,const char *cheminSprite,int x,int y,int w,int h){
+    Entity* rEntity = malloc(sizeof(Entity));
+    rEntity->sprite=createImage(cheminSprite,screen);
+    rEntity->physic.x=x;
+    rEntity->physic.y=y;
+    if(w==-1){ //Si -1 est fournit en parametre on conserve la largeur de base de l'image
+        rEntity->physic.w=getSizeTexture(rEntity->sprite).x;
+    }
+    else{
+        rEntity->physic.w=w;
+    }
+    if(h==-1){ //Si -1 est fournit en parametre on conserve la hauteur de base de l'image
+            rEntity->physic.h=getSizeTexture(rEntity->sprite).y;
+    }
+    else{
+            rEntity->physic.h=h;
+    }
+    return rEntity;
+}
+
+void printEntity(SDL_Renderer* screen,Entity* entity){
+    SDL_RenderCopy(screen,entity->sprite,NULL,&(entity->physic));
+}
+
+
